55c9bb6c574d2dbfe7df0fadc1231130b14059ad
kevinbird61 <kevinbird61@gmail.com>
2015-10-13 17:51:25 +0800

    Complete the 'q2' quiz coding implementation.
    1.Make the implement of iterative and recursive version.
    2.Not make the analysis yet.
    3.Implement with force to input "sorted array" as input.

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,47 @@
-/* FIXME: Implement! */
-
+#include <stdio.h>
+#include <string.h>
+char smallest_character(char str[],char c);
 int main()
 {
+	/* setting variable */
+	int i = 0;
+	char c,judge_c,result_c;
+	char array[10];
+	memset(array,'\0',sizeof(array));
+	/* Input array */
+	printf("Input the sorted character (input \"!\" will end the input array):\n");
+	while(i<10){
+		scanf("%c",&c);
+		if(c=='!'){
+			printf("End the Input!\n");
+			scanf("%c",&c); // For '\n'
+			break;
+		}
+		else if(c!='\n'){
+			array[i] = c;
+			i++;
+		}
+	}
+	i=0;
+	printf("Ok the array is full or you enter !\n");
+	printf("length is :%d\n",sizeof(array));
+	/* Get judgement character */
+	printf("Please input the judgement character:\n");
+	scanf("%c",&judge_c);
+	/* Implement smallest_character */
+	result_c = smallest_character(array,judge_c);
+	printf("Output: [%c]\n",result_c);
     return 0;
+}
+
+char smallest_character(char str[],char c)
+{
+	int i = 0,len = sizeof(str);
+	while(i<=len){
+		if(str[i]>c){
+			return str[i];
+		}
+		i++;
+	}
+	return str[0];
 }
--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,47 @@
-/* FIXME: Implement! */
-
+#include <stdio.h>
+#include <string.h>
+char smallest_character_recursive(char str[],char c,int index);
 int main()
 {
+		/* setting variable */
+	int i = 0;
+	char c,judge_c,result_c;
+	char array[10];
+	memset(array,'\0',sizeof(array));
+	/* Input array */
+	printf("Input the sorted character (input \"!\" will end the input array):\n");
+	while(i<10){
+		scanf("%c",&c);
+		if(c=='!'){
+			printf("End the Input!\n");
+			scanf("%c",&c); // For '\n'
+			break;
+		}
+		else if(c!='\n'){
+			array[i] = c;
+			i++;
+		}
+	}
+	i=0;
+	printf("Ok the array is full or you enter !\n");
+	printf("length is :%d\n",sizeof(array));
+	/* Get judgement character */
+	printf("Please input the judgement character:\n");
+	scanf("%c",&judge_c);
+	/* Implement smallest_character */
+	result_c = smallest_character_recursive(array,judge_c,0);
+	printf("Output: [%c]\n",result_c);
     return 0;
 }
+
+char smallest_character_recursive(char str[],char c,int index)
+{
+	if(str[index]>c){return str[index];}
+	else if(str[index]!='\0'){
+		smallest_character_recursive(str,c,++index);
+	}
+	else{
+		return str[0];
+	}
+}
+
