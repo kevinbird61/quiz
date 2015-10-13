ff16e54740351a0861f4e18bd86f6734b99acaea
kevinbird61 <kevinbird61@gmail.com>
2015-10-14 07:57:02 +0800

    Upload for the version of using makefile to estimating the cache misses.
    BreakPoint:
    I learn how to use the makefile to compile and let the action of evaluation
    more automatic.
    I think because my measured resources is using the same character(all using 'h')
    Although it ran for 1000 times , it's cache misses still low.
    But 1000 times still can see , Iterative's cache miss > Recursive's version.

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,47 +1,46 @@
 #include <stdio.h>
 #include <string.h>
+
 char smallest_character(char str[],char c);
-int main()
+int main(int argc,char *argv[])
 {
-	/* setting variable */
-	int i = 0;
-	char c,judge_c,result_c;
-	char array[10];
-	memset(array,'\0',sizeof(array));
-	/* Input array */
-	printf("Input the sorted character (input \"!\" will end the input array):\n");
-	while(i<10){
-		scanf("%c",&c);
-		if(c=='!'){
-			printf("End the Input!\n");
-			scanf("%c",&c); // For '\n'
-			break;
-		}
-		else if(c!='\n'){
-			array[i] = c;
-			i++;
-		}
-	}
-	i=0;
-	printf("Ok the array is full or you enter !\n");
-	printf("length is :%d\n",sizeof(array));
-	/* Get judgement character */
-	printf("Please input the judgement character:\n");
-	scanf("%c",&judge_c);
-	/* Implement smallest_character */
-	result_c = smallest_character(array,judge_c);
-	printf("Output: [%c]\n",result_c);
+    /* setting variable */
+    char judge_c,result_c;
+    /* Sorted array */
+    char array[15];
+    /* Set a static array for multiply input */
+    array[0] = 'a';
+    array[5] = 'i';
+    array[10] = 'q';
+    array[1] = 'c';
+    array[6] = 'k';
+    array[11] = 's';
+    array[2] = 'd';
+    array[7] = 'l';
+    array[12] = 't';
+    array[3] = 'f';
+    array[8] = 'n';
+    array[13] = 'v';
+    array[4] = 'h';
+    array[9] = 'o';
+    array[14] = 'z';
+    /* Get judgement character */
+    judge_c = *argv[1];
+    printf("%c\n",judge_c);
+    /* Implement smallest_character */
+    result_c = smallest_character(array,judge_c);
+    printf("Output: [%c]\n",result_c);
     return 0;
 }
 
 char smallest_character(char str[],char c)
 {
-	int i = 0,len = sizeof(str);
-	while(i<=len){
-		if(str[i]>c){
-			return str[i];
-		}
-		i++;
-	}
-	return str[0];
+    int i = 0;
+    while(str[i]!=NULL) {
+        if(str[i]>c) {
+            return str[i];
+        }
+        i++;
+    }
+    return str[0];
 }
