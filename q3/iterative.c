12277b3ffb2e2b1bd2f3af2476b513ac7314f456
kevinbird61 <kevinbird61@gmail.com>
2015-10-14 22:56:04 +0800

    Upload for iterative and recursive version code
    Still haven't done any analysis yet.
    For the Testing data : a left-tilt binary tree.
    And the recursive code is stilling need to optimize
    bacause it could only do the left-tilt binary tree.

--- q3/iterative.c
+++ q3/iterative.c
@@ -1,6 +1,101 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _Binary_Tree {
+    int value;
+    struct _Binary_Tree *left;
+    struct _Binary_Tree *right;
+} BT;
+
+BT *concat;
+void Init(BT *root,int append);
+void Print(BT *root);
+void flatten_iter(BT *root);
 
 int main()
 {
+    BT *root = malloc(sizeof(BT));
+    concat = malloc(sizeof(BT));
+    root->left = NULL;
+    root->right = NULL;
+    int i;
+    /* Build the Example Tree*/
+    for(i=1; i<=6; i++) {
+        Init(root,i);
+    }
+    /* Print Test */
+    Print(root);
+    /* flatten the Tree */
+    flatten_iter(root);
+    /* Check it */
+    printf("=============================\n");
+    Print(root);
+
     return 0;
 }
+
+
+/* Function Implementation */
+
+void Init(BT *root,int append)
+{
+    BT *temp = malloc(sizeof(BT));
+    BT *stand = malloc(sizeof(BT));
+    temp = root;
+    if(temp->left!=NULL) {
+        if(temp->right!=NULL) {
+            temp = temp->left;
+            Init(temp,append);
+        } else {
+            temp->right = stand;
+            stand->value = append;
+            stand->left = NULL;
+            stand->right = NULL;
+            return;
+        }
+    } else {
+        temp->left = stand;
+        stand->value = append;
+        stand->left = NULL;
+        stand->right = NULL;
+        return;
+    }
+}
+
+void Print(BT *root)
+{
+    if(root->left!=NULL) {
+        printf("Left Node is %d\n",root->value);
+        Print(root->left);
+    } else {
+        printf("End Leaf Node is %d\n",root->value);
+        return;
+    }
+    if(root->right!=NULL) {
+        printf("Right Node is %d\n",root->value);
+        Print(root->right);
+    } else {
+        printf("End Leaf Node is %d\n",root->value);
+        return;
+    }
+}
+
+/* This Function can only use on left-side binary tree */
+void flatten_iter(BT *root)
+{
+    concat = root;
+    BT *temp;
+    while(concat->left != NULL) {
+        if(concat->right!=NULL) {
+            temp = concat->right;
+            concat->right = NULL;
+            temp->left = concat->left;
+            concat->left = temp;
+            concat = concat->left;
+        } else {
+            concat = concat->left;
+        }
+    }
+}
+
+
--- q3/recursive.c
+++ q3/recursive.c
@@ -1,6 +1,101 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _Binary_Tree {
+    int value;
+    struct _Binary_Tree *left;
+    struct _Binary_Tree *right;
+} BT;
+
+BT *concat;
+void Init(BT *root,int append);
+void Print(BT *root);
+void flatten_recur(BT *root);
 
 int main()
 {
+    BT *root = malloc(sizeof(BT));
+    concat = malloc(sizeof(BT));
+    root->left = NULL;
+    root->right = NULL;
+    int i;
+    /* Build the Example Tree*/
+    for(i=1; i<=6; i++) {
+        Init(root,i);
+    }
+    /* Print Test */
+    Print(root);
+    /* flatten the Tree */
+    flatten_recur(root);
+    /* Check it */
+    printf("=============================\n");
+    Print(root);
+
     return 0;
 }
+
+
+/* Function Implementation */
+
+void Init(BT *root,int append)
+{
+    BT *temp = malloc(sizeof(BT));
+    BT *stand = malloc(sizeof(BT));
+    temp = root;
+    if(temp->left!=NULL) {
+        if(temp->right!=NULL) {
+            temp = temp->left;
+            Init(temp,append);
+        } else {
+            temp->right = stand;
+            stand->value = append;
+            stand->left = NULL;
+            stand->right = NULL;
+            return;
+        }
+    } else {
+        temp->left = stand;
+        stand->value = append;
+        stand->left = NULL;
+        stand->right = NULL;
+        return;
+    }
+}
+
+void Print(BT *root)
+{
+    if(root->left!=NULL) {
+        printf("Left Node is %d\n",root->value);
+        Print(root->left);
+    } else {
+        printf("End Leaf Node is %d\n",root->value);
+        return;
+    }
+    if(root->right!=NULL) {
+        printf("Right Node is %d\n",root->value);
+        Print(root->right);
+    } else {
+        printf("End Leaf Node is %d\n",root->value);
+        return;
+    }
+}
+
+void flatten_recur(BT *root)
+{
+    if(root->left!=NULL) {
+        concat = root->left;
+        flatten_recur(root->left);
+    } else {
+        return;
+    }
+    if(root->right!=NULL) {
+        concat->left = root->right;
+        root->right = NULL;
+        concat = concat->left;
+        flatten_recur(concat);
+    } else {
+        return;
+    }
+}
+
+
