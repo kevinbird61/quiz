6ac1544a1ef8ffa211df17a359e1efb11a47dfb4
kevinbird61 <kevinbird61@gmail.com>
2015-10-14 22:53:50 +0800

    Upload the iterative and recursive version code.
    Still haven't done any analysis yet.

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,27 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+
+int maxSubArray(int A[],int n);
 
 int main()
 {
+    /* Variable & Given Array */
+    int array[9] = {-2,1,-3,4,-1,2,1,-5,4};
+    printf("The max sum of array : %d\n",maxSubArray(array,9));
     return 0;
+}
+
+int maxSubArray(int A[],int n)
+{
+    int i,sum=0,standard=A[0];
+    for(i=0; i<n; i++) {
+        sum+=A[i];
+        if(sum<=standard) {
+            sum-=A[i];
+            if(sum>standard) {
+                standard = sum;
+            }
+            sum = 0;
+        }
+    }
+    return standard;
 }
--- q4/recursive.c
+++ q4/recursive.c
@@ -1,6 +1,35 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+
+int maxSubArray_re(int A[],int n);
+
+int sum = 0;
+int ind,standard;
 
 int main()
 {
+    /* Variable & Given Array */
+    int array[9] = {-2,1,-3,4,-1,2,1,-5,4};
+
+    standard = array[0];
+    ind = 0;
+    maxSubArray_re(array,8);
+    printf("The max sum of array : %d\n",standard);
     return 0;
+}
+
+int maxSubArray_re(int A[],int n)
+{
+    if(n!=0) {
+        maxSubArray_re(A,n-1);
+    }
+    sum+=A[n];
+    if(sum<=standard) {
+        sum-=A[n];
+        if(sum>standard) {
+            standard = sum;
+        }
+        sum = 0;
+    }
+    return standard;
+
 }
