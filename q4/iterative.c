fc7083dc0eedd3acf98147b0cb092ee894142c21
kevinbird61 <kevinbird61@gmail.com>
2015-10-18 14:58:43 +0800

    Upload For Evaluating Function about cache-misses , Time analysis.
    Using Makefile to give the testing value automatically.
    And using the gnuplot to plot the figure out.

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
-
+#include <time.h>
 int maxSubArray(int A[],int n);
 
-int main()
+int main(int argc,char *argv[])
 {
+    FILE *fp = fopen("analysis_time_iterative.txt","a");
+    clock_t start,end;
+    double result;
+    start = clock();
     /* Variable & Given Array */
     int array[9] = {-2,1,-3,4,-1,2,1,-5,4};
     printf("The max sum of array : %d\n",maxSubArray(array,9));
+    end = clock();
+    result = (end-start)/(double)(CLOCKS_PER_SEC);
+    printf("%lf\n", result);
+    if(argv[1][1]!='\0') {
+        fprintf(fp,"%c%c		%lf\n",argv[1][0],argv[1][1],result);
+    } else {
+        fprintf(fp,"%c		%lf\n",argv[1][0],result);
+    }
     return 0;
 }
 
--- q4/recursive.c
+++ q4/recursive.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
+#include <time.h>
 
 int maxSubArray_re(int A[],int n);
 
 int sum = 0;
 int ind,standard;
 
-int main()
+int main(int argc,char *argv[])
 {
+    FILE *fp = fopen("analysis_time_recursive.txt","a");
+    clock_t start,end;
+    double result;
+    start = clock();
     /* Variable & Given Array */
     int array[9] = {-2,1,-3,4,-1,2,1,-5,4};
-
     standard = array[0];
     ind = 0;
     maxSubArray_re(array,8);
     printf("The max sum of array : %d\n",standard);
+    end = clock();
+    result = (end-start)/(double)(CLOCKS_PER_SEC);
+    printf("%lf\n", result);
+    if(argv[1][1]!='\0') {
+        fprintf(fp,"%c%c		%lf\n",argv[1][0],argv[1][1],result);
+    } else {
+        fprintf(fp,"%c		%lf\n",argv[1][0],result);
+    }
     return 0;
 }
 
